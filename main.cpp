c0663b3bbb4c8f290d3492f0bb741494d98c592e
AndersonJW <u202016154@upc.edu.pe>
2021-09-05 18:40:07 -0500

    Se agrego la libreria InfInt.h. Se creo la clase RSAFUNCIONES

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include<iostream>
 #include<stdlib.h>
+#include<string>
+#include<vector>
+#include"InfInt.h"
+/*
+    Estamos usando una libreria de GitHub para poder obtener una clase
+    llamada InfInt ya que esta clase no ayudara a almacenar enteros con 
+    mayor tamaño. Los long int no tiene el espacio suficiente para poder
+    almacenar los primos que necesitamos.
+*/
 using namespace std;
+/*
+    La estructura RSAFUNCIONES sera con funciones staticas 
+    porque no necesitaremos crear objetos, solo usar sus funciones
+*/
+struct RSAFUNCIONES{
+    static void Generar(){
+        InfInt primo1,primo2,n,fi,cpublica,cprivada;
+        cout<<"Escribe el primer numero primo";cin>>primo1;
+        cout<<"Escribre el segundo numero primo";cin>>primo2;
+
+    }
+    static vector<InfInt> Encriptar(string mensaje,InfInt modulo,InfInt e){
+
+    }
+    static string Desencriptar(string mensaje,InfInt modulo,InfInt e){
+
+    }
+};
+
 int main(){
+    //MENU DE OPCIONES
     while (true)
     {
         unsigned short opcion;
