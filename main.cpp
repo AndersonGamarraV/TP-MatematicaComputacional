7cc15f9789597147cf4a36fa4cdf788c743f8469
AndersonJW <u202016154@upc.edu.pe>
2021-09-05 18:16:56 -0500

    Se agrego el menu de opciones en consola

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main(){
+    while (true)
+    {
+        unsigned short opcion;
+        cout<<"1.-Generar Claves\n2.-Encriptar\n3.-Desencriptar\n4.-Salir"<<endl;
+        cout<<"¿Que quieres hacer? ";
+        cin>>opcion;
+        switch (opcion)
+        {
+        case 1: 
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            return 1;
+        }
+    }
+    
+    return 0;
+}
